e6dd952a8adbb47c3f57f10385241f88e63a1256
PyromancerBoom <priyanshmagic@gmail.com>
2022-01-31 14:24:29 +0530

    beginning

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("this is js");
+
+const name = 'Someone';
+const age = 30;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z; // Also undefined
+
+console.log(typeof age);
